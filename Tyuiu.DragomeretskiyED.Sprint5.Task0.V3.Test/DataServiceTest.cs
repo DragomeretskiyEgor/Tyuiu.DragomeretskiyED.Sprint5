f288570bbc7727ea9393f0a94c0fd9ce6749b8a7
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-11-19 15:25:36 +0500

    Task0

--- Tyuiu.DragomeretskiyED.Sprint5.Task0.V3.Test/DataServiceTest.cs
+++ Tyuiu.DragomeretskiyED.Sprint5.Task0.V3.Test/DataServiceTest.cs
@@ -11,7 +11,7 @@ namespace Tyuiu.DragomeretskiyED.Sprint5.Task0.V3.Test
     public class DataServiceTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void ValidSaveToFileTextData()
         {
 
             string path = @"C:\Users\Gogr-PC\source\repos\Tyuiu.DragomeretskiyED.Sprint5\Tyuiu.DragomeretskiyED.Sprint5.Task0.V3\bin\Debug\OutPutFileTask0.txt";
