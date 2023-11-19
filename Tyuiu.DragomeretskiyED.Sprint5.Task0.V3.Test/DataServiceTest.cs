f710c9fbdf9c5366002482b751fd1d7434f02326
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-11-19 15:10:49 +0500

    Task0

---
+++ Tyuiu.DragomeretskiyED.Sprint5.Task0.V3.Test/DataServiceTest.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using System.IO;
+
+using Tyuiu.DragomeretskiyED.Sprint5.Task0.V3.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint5.Task0.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            string path = @"C:\Users\Gogr-PC\source\repos\Tyuiu.DragomeretskiyED.Sprint5\Tyuiu.DragomeretskiyED.Sprint5.Task0.V3\bin\Debug\OutPutFileTask0.txt";
+
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExist = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExist);
+
+        }
+    }
+}
---
+++ Tyuiu.DragomeretskiyED.Sprint5.Task0.V3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.DragomeretskiyED.Sprint5.Task0.V3.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint5.Task0.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 3;
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #5 | Выполнил Драгомерецкий Е.Д. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание №0                                                              *");
+            Console.WriteLine("* Вариант №3                                                              *");
+            Console.WriteLine("* Выполнил: Драгомерецкий Егор Дмитриевич | СМАРТб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Дано выражение вычислить его значение при x = 3                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* y = - 1/4(x^3 - 3x^2 + 4); X = 3                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Результат:                                                              *");
+
+            string res = ds.SaveToFileTextData(x);
+
+            Console.WriteLine("Файл " + res);
+            Console.WriteLine("Создан!");
+            Console.WriteLine("***************************************************************************");
+            Console.ReadKey();
+
+
+        }
+    }
+}
