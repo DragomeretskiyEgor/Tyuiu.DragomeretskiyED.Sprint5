138f649311d996e1e3868ed4ed6f3e655d2153fe
Tyuiu.DragomeretskiyED@outlook.com <mrtvinksi@mail.ru>
2023-11-26 11:10:58 +0500

    Task5

---
+++ Tyuiu.DragomeretskiyED.Sprint5.Task5.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.DragomeretskiyED.Sprint5.Task5.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.DragomeretskiyED.Sprint5.Task5.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5370bf4c-04aa-41ed-b618-8376743ed01a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.DragomeretskiyED.Sprint5.Task5.V11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+using Tyuiu.DragomeretskiyED.Sprint5.Task5.V11.Lib;
+
+namespace Tyuiu.DragomeretskiyED.Sprint5.Task5.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataService service1 = new DataService();
+            Console.Title = "Спринт #5 | Выполнил: Драгомерецкий Е.Д. | СМАРТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема:Оператор цикла continue/break                                      *");
+            Console.WriteLine("* Задание #5.5                                                            *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнил: Драгомерецкий Егор Дмитриевич | СМАРТб-23-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V11.txt";
+            Console.WriteLine("Данные в файле:" + path);
+            Console.WriteLine("File path: " + path);
+            Console.WriteLine(String.Join(" ", File.ReadAllLines(path)));
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(service1.LoadFromDataFile(path));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
